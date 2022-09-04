eccea90b3d0f15826021daaa2fb91b15b3c02f3c
Saket Ranjan <saketranjan2002@gmail.com>
2022-09-04 18:34:33 +0530

    First Commit

---
+++ public/db/conn.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const DATABASE_NAME = 'group_27'
+const CONNECTION_URL = `mongodb://localhost:27017/${DATABASE_NAME}`
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connection Successful');
+}).catch((e) => {
+    console.log(e);
+})
